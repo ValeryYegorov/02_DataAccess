163ff07c097549b8d61fd3b828a61c53f66c6d68
Valery Yegorov <valery.yegorov@mobiline.com>
2026-01-08 07:59:05 -0500

    first

---
+++ BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Contracts;
+
+namespace DataAccess;
+
+public class BaseRepository : IBaseRepository
+{
+    public HelloReply GetHelloReply(string requestName)
+    {
+        return new HelloReply
+        {
+            Message = "Hello " + requestName
+        };
+    }
+}
